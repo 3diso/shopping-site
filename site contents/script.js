ae3a713c1dcb05c7fdaa591d59307926ef69e0ac
3diso <139886201+3diso@users.noreply.github.com>
2023-07-20 09:40:57 +0100

    Add files via upload

---
+++ site contents/script.js
@@ -0,0 +1,5 @@
+/*
+function changecolor(){
+    document.getElementsByClassName("rightanswer").style.color = "darkgreen";
+    document.getElementsByClassName("answer").style.color = "red";
+  } */
