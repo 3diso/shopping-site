f9b11e19deaf2a11e7a65023c02ef04c37c57a1d
Ti Windows <>
2023-07-20 11:54:51 +0100

    button changes

--- site contents/script.js
+++ site contents/script.js
@@ -2,3 +2,6 @@ function fetchitem() {
   let name = document.getElementById("computer-title").innerHTML;
   document.getElementById("item1").innerHTML = name;
 }
+function itemdata(item){
+  window.open("./item-data.hmtl", "_blank")
+}
