10a2e7cfc044aa79a48825d34ba7abf3bb04d12e
3diso <139886201+3diso@users.noreply.github.com>
2023-07-21 14:02:57 +0100

    Update script.js

--- site contents/script.js
+++ site contents/script.js
@@ -1,23 +1,49 @@
 function fetchitem() {
-  let i = localStorage.getItem("itemNum")
-  for (i;i>0;i--){
-    let element = document.createElement("li")
-    element.innerHTML = localStorage.getItem(`item${i}`)
-    document.getElementById("basket").appendChild(element)
+  let Arr = localStorage.getItem("itemArr")
+  let arr = []
+  arr = Arr.split(',')
+  for(i=0;i<arr.length;i++){
+    if (Arr.length >0)
+    {
+      let element = document.createElement("li")
+      element.innerHTML = arr[i]
+      document.getElementById("basket1").append(element)
+    }
   }
+  localStorage.setItem("itemArr",[])
+  itemBasket = []
 }
-function setconsts(){
-  localStorage.setItem("itemNum", 1)
-}
+var itemBasket = []
+localStorage.setItem("itemArr", [])
+
 function itemdata(item){
   window.open("./item-data.html", "_blank")
   localStorage.setItem("item", item);
 }
 function printitem(item){
   document.getElementById("itemdataTitle").innerHTML = item
+  if (item == "computer")
+  {
+    document.getElementById("main").innerHTML = "computer is good"
+  } else if (item == "banana")
+  {
+    document.getElementById("main").innerHTML = "book is readable"
+  }else 
+  {
+    document.getElementById("main").innerHTML = "banana exits"
+  }
 }
-function addToBasket(item, price){
-  localStorage.setItem(`item${localStorage.getItem("itemNum")}`, item)
-  localStorage.setItem(`price${localStorage.getItem("itemNum")}`, price)
-  localStorage.setItem("itemNum", localStorage.getItem("itemNum")+1)
+function gotoBasket(){
+  document.getElementById("main").hidden = true;
+  fetchitem();
+  document.getElementById("basket").hidden = false
+}
+function gotoMain(){
+  document.getElementById("main").hidden = false;
+  document.getElementById("basket").hidden = true;
+  
+}
+function addtobasket(item, price){
+  itemBasket.push(item)
+  localStorage.setItem("itemArr", itemBasket)
 }
